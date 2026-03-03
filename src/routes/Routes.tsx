f3097439b5444a663c9d4dc275960b9ba948f34a
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-03-03 22:13:16 +0200

    refactor: apply lazy loading to all routes for code splitting

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,64 +1,73 @@
-import Home from "@/pages/Home";
-import Actor from "@/pages/Actor";
-import Movie from "@/pages/Movie";
-import TVShow from "@/pages/TVShow";
-import Session from "@/pages/Session";
-import Kids from "@/pages/Kids";
-import NewPopular from "@/pages/NewPopular";
-import MyList from "@/pages/MyList";
-import NotFound from "@/pages/NotFound";
+import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 
+// Main pages
+const Home = lazy(() => import("../pages/Home"));
+const Actor = lazy(() => import("@/pages/Actor"));
+const Movie = lazy(() => import("@/pages/Movie"));
+const TVShow = lazy(() => import("@/pages/TVShow"));
+const Session = lazy(() => import("@/pages/Session"));
+const Kids = lazy(() => import("@/pages/Kids"));
+const NewPopular = lazy(() => import("@/pages/NewPopular"));
+const MyList = lazy(() => import("@/pages/MyList"));
+const NotFound = lazy(() => import("@/pages/NotFound"));
+
 // Footer pages
-import FAQ from "@/pages/FAQ";
-import HelpCenter from "@/pages/HelpCenter";
-import Account from "@/pages/Account";
-import MediaCenter from "@/pages/MediaCenter";
-import InvestorRelations from "@/pages/InvestorRelations";
-import Jobs from "@/pages/Jobs";
-import WaysToWatch from "@/pages/WaysToWatch";
-import TermsOfUse from "@/pages/TermsOfUse";
-import Privacy from "@/pages/Privacy";
-import CookiePreferences from "@/pages/CookiePreferences";
-import CorporateInformation from "@/pages/CorporateInformation";
-import ContactUs from "@/pages/ContactUs";
-import SpeedTest from "@/pages/SpeedTest";
-import LegalNotices from "@/pages/LegalNotices";
-import OnlyOnNetflix from "@/pages/OnlyOnNetflix";
+const FAQ = lazy(() => import("@/pages/FAQ"));
+const HelpCenter = lazy(() => import("@/pages/HelpCenter"));
+const Account = lazy(() => import("@/pages/Account"));
+const MediaCenter = lazy(() => import("@/pages/MediaCenter"));
+const InvestorRelations = lazy(() => import("@/pages/InvestorRelations"));
+const Jobs = lazy(() => import("@/pages/Jobs"));
+const WaysToWatch = lazy(() => import("@/pages/WaysToWatch"));
+const TermsOfUse = lazy(() => import("@/pages/TermsOfUse"));
+const Privacy = lazy(() => import("@/pages/Privacy"));
+const CookiePreferences = lazy(() => import("@/pages/CookiePreferences"));
+const CorporateInformation = lazy(() => import("@/pages/CorporateInformation"));
+const ContactUs = lazy(() => import("@/pages/ContactUs"));
+const SpeedTest = lazy(() => import("@/pages/SpeedTest"));
+const LegalNotices = lazy(() => import("@/pages/LegalNotices"));
+const OnlyOnNetflix = lazy(() => import("@/pages/OnlyOnNetflix"));
+
 
 export default function AppRoutes() {
   return (
     <AnimatePresence mode="wait">
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/tv-shows" element={<TVShow />} />
-      <Route path="/movies" element={<Movie />} />
-      <Route path="/kids" element={<Kids />} />
-      <Route path="/new-popular" element={<NewPopular />} />
-      <Route path="/my-list" element={<MyList />} />
-      <Route path="/actor" element={<Actor />} />
-      <Route path="/session" element={<Session />} />
-      
-      {/* Footer pages */}
-      <Route path="/faq" element={<FAQ />} />
-      <Route path="/help-center" element={<HelpCenter />} />
-      <Route path="/account" element={<Account />} />
-      <Route path="/media-center" element={<MediaCenter />} />
-      <Route path="/investor-relations" element={<InvestorRelations />} />
-      <Route path="/jobs" element={<Jobs />} />
-      <Route path="/ways-to-watch" element={<WaysToWatch />} />
-      <Route path="/terms-of-use" element={<TermsOfUse />} />
-      <Route path="/privacy" element={<Privacy />} />
-      <Route path="/cookie-preferences" element={<CookiePreferences />} />
-      <Route path="/corporate-information" element={<CorporateInformation />} />
-      <Route path="/contact-us" element={<ContactUs />} />
-      <Route path="/speed-test" element={<SpeedTest />} />
-      <Route path="/legal-notices" element={<LegalNotices />} />
-      <Route path="/only-on-netflix" element={<OnlyOnNetflix />} />
-      
-      <Route path="*" element={<NotFound />} />
-      </Routes>
-      </AnimatePresence>
+      <Suspense fallback="loading...">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tv-shows" element={<TVShow />} />
+          <Route path="/movies" element={<Movie />} />
+          <Route path="/kids" element={<Kids />} />
+          <Route path="/new-popular" element={<NewPopular />} />
+          <Route path="/my-list" element={<MyList />} />
+          <Route path="/actor" element={<Actor />} />
+          <Route path="/session" element={<Session />} />
+
+          {/* Footer pages */}
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/help-center" element={<HelpCenter />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/media-center" element={<MediaCenter />} />
+          <Route path="/investor-relations" element={<InvestorRelations />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/ways-to-watch" element={<WaysToWatch />} />
+          <Route path="/terms-of-use" element={<TermsOfUse />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/cookie-preferences" element={<CookiePreferences />} />
+          <Route
+            path="/corporate-information"
+            element={<CorporateInformation />}
+          />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/speed-test" element={<SpeedTest />} />
+          <Route path="/legal-notices" element={<LegalNotices />} />
+          <Route path="/only-on-netflix" element={<OnlyOnNetflix />} />
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </AnimatePresence>
   );
 }
