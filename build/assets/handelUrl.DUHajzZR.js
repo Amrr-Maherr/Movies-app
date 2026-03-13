ac4d31f0907382ddf986b6c412b34a64c41a3a5d
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-03-13 22:29:38 +0200

    build: update production build assets

--- build/assets/handelUrl.DUHajzZR.js
+++
@@ -1,1 +0,0 @@
-import{e}from"./react-vendor.CbDLXvlW.js";const o=()=>{const o=e();return({lang:e,set:r,key:s})=>{e&&(r(e.code.toLocaleLowerCase()),o({search:`?${s}=${e.code}`}))}};export{o as u};
---
+++ build/assets/handelUrl.DvXWAbnd.js
@@ -0,0 +1,1 @@
+import{j as e}from"./react-vendor.BDDzpujV.js";const o=()=>{const o=e();return({lang:e,set:r,key:s})=>{e&&(r(e.code.toLocaleLowerCase()),o({search:`?${s}=${e.code}`}))}};export{o as u};
