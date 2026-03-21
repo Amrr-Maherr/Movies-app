58462eb733785bc05de4cd65a1b08f9d09b5c71a
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-03-21 16:09:16 +0200

    update

--- src/middleware/ProtectedRoute.tsx
+++ src/middleware/ProtectedRoute.tsx
@@ -2,10 +2,13 @@ import { Navigate, Outlet } from "react-router-dom";
 
 export default function ProtectedRoute() {
   const token = localStorage.getItem("token");
+  const payment = localStorage.getItem("paymentStatus");
+  console.log(payment, "payment");
+  console.log(token, "token");
 
-  // if (!token) {
-  //   return <Navigate to="/login" replace />;
-  // }
+  if (!token && payment !== "success") {
+    return <Navigate to="/login" replace />;
+  }
 
   return <Outlet />;
 }
